40d389c729fe701e7f8d6860110099a70980d511
milorad314 <74272463+milorad314@users.noreply.github.com>
2023-01-18 14:39:09 +0100

    ReportsImplementation

---
+++ source/Reports_Component/Interface/Impl/ReportsImpl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace Reports_Component.Interface.Impl
+{
+    class ReportsImpl : IReports
+    {
+        public void getMonthbyId()
+        {
+            string query = "select id_brojila, usage, month  from usageBrojilo";
+            List<DataSet> dataList = new List<DataSet>();
+
+            using (IDbConnection connection = InternalDataCollectionBase.
+            {
+                {
+                    connection.Open();
+                    using (IDbCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = query;
+                        command.Prepare();
+
+                        using (IDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                DataSet data = new DataSet(reader.GetInt32(0), reader.GetDecimal(1), reader.GetString(2));
+                                dataList.Add(data);
+                                
+                            }
+                        }
+                    }
+                }
+                return dataList;
+            }
+        
+    }
+
+        public void getMonthbyStreet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
