6ecb17e061605dbe105afe6ebe1d52fc9ced6132
Aleksandar Vukomanovic <avukomanovic1@gmail.com>
2023-01-18 14:19:55 +0100

    Update

---
+++ source/Replicator_Sender_Component/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replicator_Sender_Component
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
